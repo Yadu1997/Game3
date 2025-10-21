a4da0156769af3588478266e61c56db31c3eb36c
Yadu1997 <yadukrishnan25@gmail.com>
2025-10-21 11:02:08 +0530

    completed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newBtn = document.querySelector("#new-btn")
 let msgContainer = document.querySelector(".msg-container")
+let msg = document.querySelector("#winner")
 let turnX = true;
 const winPatterns = [
     [0,1,2],[3,4,5],[6,7,8],
@@ -12,10 +14,12 @@ boxes.forEach((box)=>{
     box.addEventListener("click",() => {
         if (turnX) {
             box.innerText = "X";
+            box.style.color = 'red';
             turnX = false;
         } else {
             box.innerText = "O";
             turnX = true;
+            box.style.color = 'blue';
         }
         box.disabled = true
         checkWinner();
@@ -38,10 +42,17 @@ const enableBoxes = () => {
 const showWinner = (winner) => {
     disableBoxes();
     resetBtn.classList.add("hide")
+    msg.innerText = `${winner} Won`
+    msgContainer.classList.remove("hide")
 }
 
 const resetGame = () => {
     turnX = true;
+    enableBoxes();
+}
+
+const newGame = () => {
+    turnX = true
     enableBoxes();
     msgContainer.classList.add("hide")
     resetBtn.classList.remove("hide")
@@ -52,12 +63,14 @@ const checkWinner = () => {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
+        let draw = true
         if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
             if (pos1Val === pos2Val && pos2Val === pos3Val) {
-                showWinner(pos1Val)
+                showWinner(pos1Val);
             }
         }
     }
 }
 
 resetBtn.addEventListener("click", resetGame);
+newBtn.addEventListener("click", newGame);
