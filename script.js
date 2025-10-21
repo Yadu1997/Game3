c2d608e5e4609391c032ce3d8d56f121f9450265
Yadu1997 <yadukrishnan25@gmail.com>
2025-10-21 11:25:40 +0530

    added a draw function

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let resetBtn = document.querySelector("#reset-btn");
 let newBtn = document.querySelector("#new-btn")
 let msgContainer = document.querySelector(".msg-container")
 let msg = document.querySelector("#winner")
+let turn = document.querySelector("#turn")
 let turnX = true;
+let count = 0
 const winPatterns = [
     [0,1,2],[3,4,5],[6,7,8],
     [0,3,6],[1,4,7],[2,5,8],
@@ -12,12 +14,15 @@ const winPatterns = [
 
 boxes.forEach((box)=>{
     box.addEventListener("click",() => {
+        count += 1
         if (turnX) {
             box.innerText = "X";
+            turn.innerText = "Turn of O"
             box.style.color = 'red';
             turnX = false;
         } else {
             box.innerText = "O";
+            turn.innerText = "turn of X"
             turnX = true;
             box.style.color = 'blue';
         }
@@ -44,11 +49,22 @@ const showWinner = (winner) => {
     resetBtn.classList.add("hide")
     msg.innerText = `${winner} Won`
     msgContainer.classList.remove("hide")
+    turn.innerText = ""
+}
+const showDraw = () => {
+    if (count == 9) {
+        msgContainer.classList.remove("hide")
+        msg.innerText = "its a draw"
+        resetBtn.classList.add("hide")
+        turn.innerText = ""
+    }
 }
 
 const resetGame = () => {
     turnX = true;
     enableBoxes();
+    count = 0
+    turn.innerText = "Turn of X"
 }
 
 const newGame = () => {
@@ -56,6 +72,8 @@ const newGame = () => {
     enableBoxes();
     msgContainer.classList.add("hide")
     resetBtn.classList.remove("hide")
+    count = 0
+    turn.innerText = "Turn of X"
 }
 
 const checkWinner = () => {
@@ -67,6 +85,9 @@ const checkWinner = () => {
         if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
             if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 showWinner(pos1Val);
+                count = 0
+            }else{
+                showDraw()
             }
         }
     }
